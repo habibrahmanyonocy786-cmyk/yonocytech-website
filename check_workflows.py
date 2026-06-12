a5f4dba93efedbccc2b4c7a19a92fe5db39c94b2
Habib <habib@yonocytech.com>
2026-06-12 04:52:14 -0700

    Add 'use client' to Footer, remove dead CSS vars, remove utility scripts

--- check_workflows.py
+++
@@ -1,5 +0,0 @@
-import sys, json
-
-data = json.load(sys.stdin)
-for r in data.get("workflow_runs", []):
-    print(f'{r["id"]} | {r["status"]:10s} | {r.get("conclusion") or "N/A":12s} | {r["event"]}')
